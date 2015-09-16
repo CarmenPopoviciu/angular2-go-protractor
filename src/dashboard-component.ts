75bf630bc0bf3ac89f4a45823585a6b7d0175522
John Papa <john@johnpapa.net>
2015-09-16 08:10:34 -0400

    removed console

--- src/dashboard-component.ts
+++ src/dashboard-component.ts
@@ -6,14 +6,16 @@ import {Component, FORM_DIRECTIVES, View} from 'angular2/angular2';
 		<h2>Dashboard</h2>
 		<div>Hello {{name}}</div>
 		<input [(ng-model)]="name" />
-		<button (click)="show()">Show in console</button>
+		<button (click)="sayHello()">Say Hello</button>
+		<p>{{message}}</p>
 	`,
 	directives: [FORM_DIRECTIVES]
 })
 export class DashboardComponent {
 	name = 'john';
+	message = '';
 
-	show() {
-		console.log(`Name = ${this.name}`);
+	sayHello() {
+		this.message = `Hello ${this.name}!`;
 	}
 }
