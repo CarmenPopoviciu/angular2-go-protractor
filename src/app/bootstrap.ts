b859a551a19b07fce0b60e3d207ecf26f6a58264
John Papa <john@johnpapa.net>
2015-10-14 08:11:54 -0400

    42

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -1,10 +1,10 @@
 import {bind, bootstrap} from 'angular2/angular2';
-import {ROUTER_BINDINGS, HashLocationStrategy, LocationStrategy} from 'angular2/router';
+import {ROUTER_PROVIDERS, HashLocationStrategy, LocationStrategy} from 'angular2/router';
 import {CharacterService} from './character.service';
 import {AppComponent} from './app.component';
 
 bootstrap(AppComponent, [
-	ROUTER_BINDINGS,
+	ROUTER_PROVIDERS,
 	CharacterService,
 	bind(LocationStrategy).toClass(HashLocationStrategy)
 ]);
