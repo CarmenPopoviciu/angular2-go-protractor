dfe17f718725bb21f437c749dc57bda6f5d41189
John Papa <john@johnpapa.net>
2015-11-01 07:33:03 -0500

    updated for ng alpha 45 and sysjs 0.19.5

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import {View, Component} from 'angular2/angular2';
+import {Component} from 'angular2/angular2';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {CharactersComponent} from './characters.component';
 import {DashboardComponent} from './dashboard.component';
 
-@Component({ selector: 'my-app' })
-@View({
+@Component({
+  selector: 'my-app',
   template: `
     <a [router-link]="['./Dashboard']">Dashboard</a>
     <a [router-link]="['./Characters']">Characters</a>
