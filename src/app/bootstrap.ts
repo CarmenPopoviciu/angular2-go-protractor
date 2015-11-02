4afd7ca454105316933dafddffcf905639931c2d
Martin Hochel <hochelmartin@gmail.com>
2015-11-02 15:23:48 +0100

    refactor(bootstrap): use provide instead of deprecated bind

--- src/app/bootstrap.ts
+++ src/app/bootstrap.ts
@@ -1,4 +1,4 @@
-import {bind, bootstrap} from 'angular2/angular2';
+import {provide, bootstrap} from 'angular2/angular2';
 import {ROUTER_PROVIDERS, HashLocationStrategy, LocationStrategy} from 'angular2/router';
 import {CharacterService} from './character.service';
 import {AppComponent} from './app.component';
@@ -6,5 +6,5 @@ import {AppComponent} from './app.component';
 bootstrap(AppComponent, [
 	ROUTER_PROVIDERS,
 	CharacterService,
-	bind(LocationStrategy).toClass(HashLocationStrategy)
+	provide(LocationStrategy, {useClass: HashLocationStrategy})
 ]);
